424095134ad09f332655a26af91df37345e7a71b
seminchoi <choicco89@gmail.com>
2025-01-13 02:11:23 +0900

    test: ConfigTests CsvSource 테스트 추가 작성

--- HourDataProcessor.Tests/ConfigTests.cs
+++ HourDataProcessor.Tests/ConfigTests.cs
@@ -8,10 +8,14 @@ public class ConfigTests
         var config = ConfigHolder.AppConfig;
         Assert.Multiple(() =>
         {
-            Assert.That(config.Database?.ConnectionString, Is.Not.Null);
             Assert.That(config.Database?.ConnectionString,
                 Is.EqualTo(
                     "Server=localhost;Database=TestLogApp;User Id=sa;Password=rootRoot123;;TrustServerCertificate=True"));
+            Assert.That(config.CsvSource?.BasePath,
+                Is.EqualTo(
+                    "resources"));
+            Assert.That(config.CsvSource?.FileNames,
+                Has.Count.EqualTo(3));
         });
     }
 }
