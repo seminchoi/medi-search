cbfa4feba1bd7a6de456e44ae7ffc9c5a8860d9b
seminchoi <choicco89@gmail.com>
2025-01-13 01:45:57 +0900

    refactor: InstitutionTypeHelper 처리 방식 개선
    - null, empty 문자열 처리 개선
    - 예상 외의 문자열 처리 개선

--- HourDataProcessor/Entity/InstitutionType.cs
+++ HourDataProcessor/Entity/InstitutionType.cs
@@ -20,9 +20,26 @@ public enum InstitutionType
 
 public static class InstitutionTypeHelper
 {
-    public static InstitutionType ValueOf(string institutionName)
+    public static InstitutionType ValueOf(string? institutionName)
     {
-        if (institutionName.Contains("병원") || institutionName.Contains("의원") || institutionName.Equals("상급종합"))
+        if (string.IsNullOrWhiteSpace(institutionName))
+            return InstitutionType.Unknown;
+
+        return institutionName switch
+        {
+            "병원" or "상급종합" or "요양병원" or "종합병원" or "의원"
+                or "정신병원" or "치과병원" or "치과의원" or "한방병원" or "한의원" => InstitutionType.Hospital,
+            "약국" => InstitutionType.DrugStore,
+            "보건소" or "보건의료원" or "보건지소" or "보건진료소" => InstitutionType.HealthCenter,
+            "조산원" => InstitutionType.MidwifeCenter,
+            _ => ProcessOtherInstitutionName(institutionName)
+        };
+    }
+
+    private static InstitutionType ProcessOtherInstitutionName(string institutionName)
+    {
+
+        if (institutionName.Contains("병원") || institutionName.Contains("의원"))
             return InstitutionType.Hospital;
 
         if (institutionName.Contains("약국"))
@@ -31,17 +48,9 @@ public static class InstitutionTypeHelper
         if (institutionName.Contains("보건"))
             return InstitutionType.HealthCenter;
 
-        if (institutionName.Contains("조산원"))
+        if (institutionName.Contains("조산"))
             return InstitutionType.MidwifeCenter;
-        
-        return institutionName switch
-        {
-            "병원" or "상급종합" or "요양병원" or "종합병원" or "의원"
-                or "정신병원" or "치과병원" or "치과의원" or "한방병원" or "한의원" => InstitutionType.Hospital,
-            "약국" => InstitutionType.DrugStore,
-            "보건소" or "보건의료원" or "보건지소" or "보건진료소" => InstitutionType.HealthCenter,
-            "조산원" => InstitutionType.MidwifeCenter,
-            _ => InstitutionType.Unknown
-        };
+
+        return InstitutionType.Unknown;
     }
 }
