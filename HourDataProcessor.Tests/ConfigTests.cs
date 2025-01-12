2f909173c3ea98b18e2927e3ba4fc891eefe3168
seminchoi <choicco89@gmail.com>
2025-01-13 01:09:31 +0900

    test: 설정 파일 변경으로 ConfigTest fail 수정

--- HourDataProcessor.Tests/ConfigTests.cs
+++ HourDataProcessor.Tests/ConfigTests.cs
@@ -11,7 +11,7 @@ public class ConfigTests
             Assert.That(config.Database?.ConnectionString, Is.Not.Null);
             Assert.That(config.Database?.ConnectionString,
                 Is.EqualTo(
-                    "Server=localhost;Database=TestLogApp;User Id=sa;Password=rootRoot123;Pooling=true;Min Pool Size=1; Max Pool Size = 4;TrustServerCertificate=True"));
+                    "Server=localhost;Database=TestLogApp;User Id=sa;Password=rootRoot123;;TrustServerCertificate=True"));
         });
     }
 }
